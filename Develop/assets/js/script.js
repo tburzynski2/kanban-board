afd3234f6c18ec3f1903993e47a4ef1213ff3d22
Tom Bee <tburzynski2@yahoo.com>
2024-04-03 18:04:15 -0500

    Added datepicker functionality to Task Due Date input field

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,4 @@
 // Retrieve tasks and nextId from localStorage
-let taskListA;
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
@@ -36,6 +35,7 @@ function createTaskCard(task) {
       </div>
     </div>
   `);
+
   return card;
 }
 
@@ -93,6 +93,9 @@ function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  $(function () {
+    taskDueDateInputEl.datepicker();
+  });
   submitButtonEl.on("click", handleAddTask);
   renderTaskList();
 });
